d31c6e5f1f0657c2388dc79537437e206331cdcc
alexandercleasby <cleasby.alex@gmail.com>
2020-01-11 18:55:54 -0500

    added and styled loading bar.

--- app/components/LoadingBar.js
+++ app/components/LoadingBar.js
@@ -1,13 +1,22 @@
 // @flow
 import React, { useState } from 'react';
 import { ipcRenderer } from 'electron';
+import Style from './loading.css';
 
 const LoadingBar = () => {
   const [completion, setCompletion] = useState(0);
   ipcRenderer.on('complettionChange', (e, percentage) =>
-    setCompletion(percentage)
+    setCompletion(parseFloat(percentage * 100).toFixed(1))
   );
-  return <div>{`loading ${completion}`}</div>;
+  if (!completion) {
+    return '';
+  }
+  return (
+    <div className={Style.bar}>
+      <div className={Style.text}>{`loading ${completion}%`}</div>
+      <div className={Style.inner} style={{ width: `${completion}%` }} />
+    </div>
+  );
 };
 
 export default LoadingBar;
